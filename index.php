7206c7ee5b22ebaf43452d51d66274c68e965174
Jordan Brauer <jbrauer.inc@gmail.com>
2017-05-24 15:50:56 -0500

    Feat: added usage examples.

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
 
-// Usage
+// Usage Examples
 
-require_once 'vendor/autoload.php';
+require_once "vendor/autoload.php";
 
-$cronitor = new \Cronitor\Monitor('i8XGrq');
+use \Cronitor\Monitor;
 
-$cronitor->run();
-$cronitor->complete();
-// $cronitor->fail();
-// $cronitor->pause(0);
+$cronitor = new Monitor("monitor_id", [
+  "base_url" => "https://cronitor.link",
+  "auth_key" => "your_private_secret_confidential_auth_key",
+]);
+
+$cronitor->run(); // plain run ping
+$cronitor->run("Hello Cronitor!"); // run ping with message
+
+$cronitor->complete(); // plain complete ping
+$cronitor->complete("Goodbye Cronitor!"); // complete ping with message
+
+$cronitor->fail(); // plain fail ping
+$cronitor->fail("Damn Cronitor!"); // fail ping with message
+
+$cronitor->pause(1); // pause for 1 hour
+$cronitor->start(); // resume monitoring
